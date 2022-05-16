ff0001f3fefd1eadbf40e8c204527f3ab7f5f86f
luanaporto21 <luana@angulodigital.com.br>
2022-05-16 17:16:54 -0300

    desafio rocketshoes finalizado.

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,5 +1,5 @@
 import { strictEqual } from 'assert';
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
@@ -33,6 +33,19 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
         return [];
     });
 
+    const prevCartRef = useRef<Product[]>();
+    useEffect(() => {
+        prevCartRef.current = cart;
+    })
+
+    const cartPreviousValue = prevCartRef.current ?? cart;
+
+    useEffect(() => {
+        if(cartPreviousValue !== cart){
+            localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+        }
+    })
+
     const addProduct = async (productId: number) => {
         try {
             const updatedCart = [...cart];
@@ -65,7 +78,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
             }
 
             setCart(updatedCart);
-            localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
 
         } catch {
             toast.error('Erro na adição do produto');
@@ -81,7 +93,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
                 updatedCart.splice(productIndex, 1);
 
                 setCart(updatedCart);
-                localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
 
             }else{
                 throw Error();
@@ -116,7 +127,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
             if(productExists){
                 productExists.amount = amount;
                 setCart(updatedCart);
-                localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
             }else{
                 throw Error();
             }           
